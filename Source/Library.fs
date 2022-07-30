092410d8ef9b77cd6ba295501d0dc70d6f48b44d
Mathias Løken <lakavi@mailfence.com>
2022-07-30 13:18:29 +0200

    Getting there...

--- Source/Library.fs
+++ Source/Library.fs
@@ -4,40 +4,161 @@ open System
 open System.IO
 open FSharp.Data
 
+type private Parser(str: string) =
+    let str = str
+    let mutable i = 0
+    let pairs = ResizeArray<string * string>()
+    let prefixes = ResizeArray<string>()
+
+    let ensure cond =
+        if not cond then
+            failwith $"Unexpected character: '{str[i]}' at position {i}"
+
+    /// Comment and whitespace skipper courtesy of https://github.com/fsprojects/FSharp.Data/blob/main/src/Json/JsonValue.fs
+    let rec skipCommentsAndWhitespace () =
+        let skipComment () =
+            // Supported comment syntax:
+            // - // ...{newLine}
+            // - /* ... */
+            if i < str.Length && str[i] = '/' then
+                i <- i + 1
+
+                if i < str.Length && str[i] = '/' then
+                    i <- i + 1
+
+                    while i < str.Length
+                          && (str[i] <> '\r' && str[i] <> '\n') do
+                        i <- i + 1
+                else if i < str.Length && str[i] = '*' then
+                    i <- i + 1
+
+                    while i + 1 < str.Length
+                          && str[i] <> '*'
+                          && str[i + 1] <> '/' do
+                        i <- i + 1
+
+                    ensure (
+                        i + 1 < str.Length
+                        && str[i] = '*'
+                        && str[i + 1] = '/'
+                    )
+
+                    i <- i + 2
+
+                true
+            else
+                false
+
+        let skipWhitespace () =
+            let initialI = i
+
+            while i < str.Length && Char.IsWhiteSpace str[i] do
+                i <- i + 1
+
+            initialI <> i // return true if some whitespace was skipped
+
+        if skipWhitespace () || skipComment () then
+            skipCommentsAndWhitespace ()
+
+
+    /// Get the first value part of a JSON string
+    let rec getValue () =
+
+        let start = i
+        let mutable inQuotes = false
+
+        while not inQuotes && (str[i] <> ',' || str[i] <> '}') do
+            if str[i] = '\\' then
+                i <- i + 2
+            else
+                i <- i + 1
+
+                if str[i] = '"' then
+                    inQuotes <- not inQuotes
+
+        let value = str[start..i]
+        printfn "value: %s" value
+        i <- i + 1
+        value
+
+    /// Get the first key part of a JSON string
+    and getKey () : string =
+        ensure (str[i] = '"')
+        let start = i
+        i <- i + 1
+
+        while str[i] <> '"' do
+            if str[i] = '\\' then
+                i <- i + 2
+            else
+                i <- i + 1
+
+        let key = str[start..i]
+        i <- i + 1
+
+        skipCommentsAndWhitespace ()
+        ensure (str[i] = ':')
+        i <- i + 1
+
+        skipCommentsAndWhitespace ()
+
+        if str[i] = '{' then
+            i <- i + 1
+            skipCommentsAndWhitespace ()
+            prefixes.Add key
+            getKey ()
+        else
+            key
+
+    /// Climb up from JSON objects levels
+    let ascend () =
+        skipCommentsAndWhitespace ()
+
+        if str[i] = ',' then
+            i <- i + 1
+            skipCommentsAndWhitespace ()
+
+        while str[i] = '}' do
+            prefixes.RemoveAt(prefixes.Count - 1)
+            i <- i + 1
+            skipCommentsAndWhitespace ()
+
+    /// Parse a JSON string into flat key/value pars
+    member _.Parse() =
+        skipCommentsAndWhitespace ()
+        ensure (str[i] = '{')
+        i <- i + 1
+
+        let stop = str.LastIndexOf '}'
+
+        while i < stop do
+            ascend ()
+            let key = getKey ()
+            printfn "key: %s" key
+            let fullKey =
+                String.Join("__", List.ofArray (prefixes.ToArray()) @ [key])
+
+            let value = getValue ()
+            printfn "\npair: %s: %s\n" fullKey value
+
+            pairs.Add(fullKey, value)
+
+        ""
+
 module Appsettings =
 
-    let private LoadFile filename =
+    /// Read file if exists
+    let private TryReadFile filename =
         let exists = File.Exists filename
 
         match exists with
         | true -> Some(File.ReadAllText filename)
         | false -> None
 
-    /// Find first comma not in quotes
-    let rec private FirstComma (str: string) (i: int32) (inQuotes: bool) (escaped: bool): int32 option =
-        try
-            match str.Chars i with
-            | ',' when not inQuotes -> Some i
-            | '"' when not escaped -> FirstComma str (i + 1) (not inQuotes) false
-            | '"' when escaped -> FirstComma str (i + 1) inQuotes false
-            | '\\' when not escaped -> FirstComma str (i + 1) inQuotes true
-            | _ -> FirstComma str (i + 1) inQuotes false
-        with
-            | :? IndexOutOfRangeException -> None
-    
-    /// Split JSON string on commas not in quotes
-    let rec private Split (str: string) (i: int32) (res: string list): string list =
-        let firstQuote = str.IndexOf('"', i)
-        match FirstComma str firstQuote false false with
-        | Some firstComma ->
-            let row = str[firstQuote..firstComma - 1]
-            printfn "%s" row
-            Split str (firstComma + 1) (res @ [row])
-        | None -> res
-    
     /// Merge two JSON strings
     let private Merge (x: string) (y: string) =
-        let rows = Split x (x.IndexOf '{') List.empty
+        let rows = Parser(x).Parse()
+        printfn "%d" rows.Length
         JsonValue.Parse "{}"
 
     /// Load appsettings
@@ -48,9 +169,9 @@ module Appsettings =
         let envJson =
             match String.IsNullOrWhiteSpace env with
             | true -> None
-            | false -> LoadFile $"appsettings.{env}.json"
+            | false -> TryReadFile $"appsettings.{env}.json"
 
-        match LoadFile "appsettings.json" with
+        match TryReadFile "appsettings.json" with
         | Some envRoot when envJson.IsSome -> Merge envRoot envJson.Value // Both merged
         | Some envRoot when envJson.IsNone -> JsonValue.Parse envRoot // Only appsettings.json
         | None when envJson.IsSome -> JsonValue.Parse envJson.Value // Only appsettings.{env}.json

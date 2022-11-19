8b9fd3cda45d25ba2ccac38b9d3c9ec0610bbfee
Mathias Løken <lakavi@mailfence.com>
2022-11-19 21:26:08 +0100

    Fetch example

--- Sandbox/Program.fs
+++ Sandbox/Program.fs
@@ -1,5 +1,7 @@
-﻿open System.Text.Json
+﻿open System
+open System.Text.Json
 open FSharp.Appsettings
+open FSharp.Data
 
 type LogLevel =
     { Default: string
@@ -7,12 +9,29 @@ type LogLevel =
       System: string }
 
 type Logging = { LogLevel: LogLevel }
-type Settings = { Logging: Logging }
+type Settings = { Logging: Logging; BaseUrl: string; }
 
 let appsettings = Appsettings.Load()
 
 printfn "Config: %s" (appsettings.ToJsonString(JsonSerializerOptions(WriteIndented = true)))
 
-let typedAppsettings = Appsettings.LoadTyped<Settings>()
+let typedAppsettings =
+    Appsettings.LoadTyped<Settings>()
 
 printfn "Typed: %A" typedAppsettings
+
+type WeatherForecast =
+    { date: DateTime
+      temperatureC: int32
+      temperatureF: int32
+      summary: Option<string> }
+
+let GetFromJson<'T> () =
+    let response =
+        Http.RequestString (Uri(Uri(typedAppsettings.BaseUrl), "/WeatherForecast").ToString())
+
+    JsonSerializer.Deserialize<'T>(response)
+
+
+GetFromJson<seq<WeatherForecast>>()
+|> Seq.iter (fun x -> printfn "%b" (x.summary.IsSome))

383bd8d983686472c32bf6dc91fe1a0242299b89
Mathias Løken <lakavi@mailfence.com>
2022-12-03 16:28:28 +0100

    URL is deprecated...

--- Sandbox/Console/ConsoleLogger.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace FSharp.Appsettings.Sandbox.Console
-
-open Microsoft.Extensions.Logging
-
-type ConsoleLogger =
-    interface ILogger with
-        member this.BeginScope(state) = failwith "todo"
-        member this.IsEnabled(logLevel) = logLevel <> LogLevel.None
-        member this.Log(logLevel, eventId, state, ``exception``, formatter) = failwith "todo"
